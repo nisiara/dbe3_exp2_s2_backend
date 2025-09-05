6ffc312bed8764b61b4b8966506379c05318330c
Nicolas Silva <nic.silvaa@duocuc.cl>
2025-09-05 16:28:35 -0400

    Set up main backend project

---
+++ src/test/java/com/bancoxyz/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancoxyz.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
