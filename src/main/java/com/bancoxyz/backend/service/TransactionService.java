c0db2119a952242c8f25a0bf3a754cb8ac941db9
Nicolas Silva <nic.silvaa@duocuc.cl>
2025-09-05 17:52:27 -0400

    Map entity to response DTO

--- src/main/java/com/bancoxyz/backend/service/TransactionService.java
+++ src/main/java/com/bancoxyz/backend/service/TransactionService.java
@@ -1,7 +1,11 @@
+package com.bancoxyz.backend.service;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.bancoxyz.backend.model.entity.Transaction;
+import com.bancoxyz.backend.model.dto.TransactionResponse;
 import com.bancoxyz.backend.repository.TransactionRepository;
 
 @Service
@@ -12,7 +16,19 @@ public class TransactionService {
   private TransactionRepository transactionRepository;
 
   public TransactionService(TransactionRepository transactionRepository) {
-      this.transactionRepository = transactionRepository;
+    this.transactionRepository = transactionRepository;
+  }
+
+  public List<TransactionResponse> findAllTransactions() {
+     return transactionRepository.findAll().stream() 
+      .map(transaction -> new TransactionResponse(
+          transaction.getId(),
+          transaction.getTransactionDate(),
+          transaction.getAmount(),
+          transaction.getType()
+      ))
+      .toList();
+      
   }
 
 }
